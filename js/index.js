925cbd8a73040b76b7b6c59c27232ce806c30da2
Jason Gomez <jason.gomez@me.com>
2019-06-05 10:13:35 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
 
 function saveLincoln(interval) {
   if(isNaN(interval)) {
-    interval = 10000
+    interval = 10000;
   }
-  $('#foreground').fadeOut(interval);
+  // $('#foreground').fadeOut(interval);
+  const fore = document.getElementById('foreground');
+  setTimeout( () => { fore.remove() } , interval);
 }
+
+const saveLincolnButton = document.getElementById('save_lincoln');
+
+saveLincolnButton.addEventListener('click', (e) => {
+  const interval = parseInt(document.getElementById('interval').value);
+  console.log(interval);
+  saveLincoln(interval);
+})
+
