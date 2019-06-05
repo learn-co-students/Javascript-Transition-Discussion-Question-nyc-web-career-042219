1c8658c856e21cb67ccadcc37cbfac0a405064b9
Carly Pecora <carly@carlypecora.com>
2019-06-05 09:47:52 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,31 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
 
 function saveLincoln(interval) {
   if(isNaN(interval)) {
     interval = 10000
   }
-  $('#foreground').fadeOut(interval);
+  var fadeTarget = document.getElementById("foreground");
+  fadeOutEffect(fadeTarget, interval);
 }
+
+function fadeOutEffect(fadeTarget, interval) {
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.3;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, interval);
+}
+
+
+var save_lincoln = document.querySelector("#save_lincoln");
+
+save_lincoln.addEventListener("click", function(){
+	let button = document.querySelector("#interval");
+	let num = parseInt(button.value)
+	saveLincoln(num);
+})
