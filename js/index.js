3abcc4b15014395473ca30f3faebbe589446d87e
Jake <jakelovitz@gmail.com>
2019-06-05 09:58:55 -0400

    Added pure JS fade out effect

--- js/index.js
+++ js/index.js
@@ -1,12 +1,29 @@
 // $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
 
 let save = document.querySelector('#save_lincoln')
+
 save.addEventListener('click', saveLincoln)
 
 
+
 function saveLincoln(interval) {
+  let fade = document.querySelector('#foreground')
   if(isNaN(interval)) {
-    interval = 10000
+    interval = 1000
   }
-  $('#foreground').fadeOut(interval);
+  fadeOutEffect();
+}
+
+function fadeOutEffect() {
+  var fadeTarget = document.querySelector("#foreground");
+  var fadeEffect = setInterval(function () {
+      if (!fadeTarget.style.opacity) {
+          fadeTarget.style.opacity = 1;
+      }
+      if (fadeTarget.style.opacity > 0) {
+          fadeTarget.style.opacity -= 0.1;
+      } else {
+          clearInterval(fadeEffect);
+      }
+  }, 200);
 }
