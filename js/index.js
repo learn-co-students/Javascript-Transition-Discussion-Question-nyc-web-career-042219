bb08947a62f6f462672da77c21a5241a10a10e15
Jennifer Oh <ohjennifer.90@gmail.com>
2019-06-06 09:18:57 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+  // $('#foreground').fadeOut(interval);
+// }
+
+
+const saveLincoln = document.querySelector('#save_lincoln')
+
+function president(interval){
+  if (isNaN(interval)){
     interval = 10000
   }
-  $('#foreground').fadeOut(interval);
+  const foreground = document.getElementById('foreground').remove()
+  return foreground
 }
+saveLincoln.addEventListener('click', function(e) {
+  const interval = parseInt(document.getElementById('interval'))
+  president(interval)
+})
+
+// document.style.background = "#000000"
